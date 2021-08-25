81705e237e1e5afa8c9f50323e47e80e217df7f5
wangpan <wp522765678@vip.qq.com>
2021-08-25 11:42:54 +0800

    提交了更新

--- assets/js/my_login.js
+++ assets/js/my_login.js
@@ -7,4 +7,66 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+    //从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    //通过form.verify()函数自定义校验规则
+    form.verify({
+        // 自定义了一个叫做pwd的规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        //校验两次密码是否一致
+        repwd: function (value) {
+            //传入确认密码框的value
+            //获取密码框的value
+            //比对两次的value是否一致
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        //阻止默认行为
+        e.preventDefault()
+        //需要提交的数据
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        // 发起POST请求
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功 请登录');
+            //模拟人的点击行为，跳转到登录页面
+            $('#link_login').click()
+        })
+    })
+    //监听注册表单的提交事件
+    $('#form_login').on('submit', function (e) {
+        //阻止默认行为
+        e.preventDefault()
+        //需要提交的数据
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        // 发起POST请求
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功');
+                localStorage.setItem('token', res.token)
+                location.href = '/my_index.html'
+            }
+        }
+        )
+    })
 })
