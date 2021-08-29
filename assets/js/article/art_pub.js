65eea3e229a15d4549dd7e62a3ec38f6563a0f20
wangpan <wp522765678@vip.qq.com>
2021-08-29 11:00:37 +0800

    发布文章模块完成

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -17,4 +17,82 @@ $(function () {
             }
         })
     }
+    // 初始化富文本编辑器
+    initEditor()
+    // 1. 初始化图片裁剪器
+    var $image = $('#image')
+
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+    // 为选择封面的按钮，绑定点击事件处理函数
+    $('#btnChooseImage').on('click', function () {
+        $('#coverFile').click()
+    })
+    // 监听`coverFile` 的`change` 事件，获取用户选择的文件列表
+    $('#coverFile').on('change', function (e) {
+        var files = e.target.files
+        if (files.length === 0) {
+            return
+        }
+        var file = e.target.files[0]
+        var newImgURL = URL.createObjectURL(file)
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+    //定义文章的发布状态
+    var art_state = '已发布'
+    //为存为草稿按钮 绑定点击事件处理函数
+    $('#btnSave2').on('click', function () {
+        art_state = '草稿'
+    })
+    //为表单绑定submit事件
+    $('#form-pub').on('submit', function (e) {
+        e.preventDefault()
+        var fd = new FormData($(this)[0])
+        // 3. 将文章的发布状态，存到 fd 中
+        fd.append('state', art_state)
+
+        $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 5. 将文件对象，存储到 fd 中
+                fd.append('cover_img', blob)
+                // 6. 发起 ajax 数据请求
+                publishArticle(fd)
+            })
+
+    })
+    //定义 发布文章的方法
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            // 注意：如果向服务器提交的是 FormData 格式的数据，
+            // 必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('发布文章失败')
+                }
+                layui.layer.msg('发布文章成功')
+                location.href = '/article/art_list.html'
+            }
+        })
+    }
 })
